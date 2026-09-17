78d756f1cbe67408c67be3067f6864cfdb98bc54
asololeq <asxbrianti@gmail.com>
2026-09-17 17:02:58 +0800

    PRAKTIKUM 4: MODEL & ELOQUENT ORM

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
+use App\Models\Mahasiswa;
+Route::get('/mahasiswa', function () {
+ $data = Mahasiswa::all();
+ return view('mahasiswa.index', compact('data'));
 });
